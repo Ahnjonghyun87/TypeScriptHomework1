cec0bbf5a3955d518f5679fe31b6762fe5842c2b
[Github <helgo1318@gmail.com>
2024-06-28 06:01:05 +0900

    완성

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,16 +1,35 @@
+import styled from "styled-components";
 import { Country } from "../types/countries";
 
 interface CountryCardProps {
   country: Country;
+  handleSelectedCountry: (country: Country) => void;
 }
 
-const CountryCard: React.FC<CountryCardProps> = ({ country }) => {
+const CountryCard: React.FC<CountryCardProps> = ({
+  country,
+  handleSelectedCountry,
+}) => {
   return (
-    <div>
+    <StCard onClick={() => handleSelectedCountry(country)}>
       <img src={country.flags.svg} style={{ width: "40px", height: "40px" }} />
       <h3>{country.name.common}</h3>
-    </div>
+    </StCard>
   );
 };
 
 export default CountryCard;
+
+const StCard = styled.div`
+  border-radius: 2px;
+  border: 1px solid black;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.2);
+  padding: 10px;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 350px;
+  height: 150px;
+`;
--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import styled from "styled-components";
 import { getCountries } from "../api/countries";
 import { Country } from "../types/countries";
 import CountryCard from "./CountryCard";
 
 const CountryList: React.FC = () => {
   const [countries, setCountries] = React.useState<Country[]>([]);
+  const [selectedCountries, setSelectedCountries] = React.useState<Country[]>(
+    []
+  );
 
   React.useEffect(() => {
     const fetchCountries = async () => {
@@ -19,16 +23,71 @@ const CountryList: React.FC = () => {
     fetchCountries();
     console.log("countries :", countries);
   }, []);
+
+  const handleSelectedCountry = (country: Country): void => {
+    if (
+      !selectedCountries.find(
+        (selectedCountry: Country) =>
+          selectedCountry.name.common === country.name.common
+      )
+    ) {
+      setSelectedCountries([...selectedCountries, country]);
+    } else {
+      setSelectedCountries(
+        selectedCountries.filter((selectedCountry: Country) => {
+          selectedCountry.name.common !== country.name.common;
+        })
+      );
+    }
+  };
+
   return (
-    <div>
-      <h1>나라목록</h1>
-      <div>
-        {countries.map((country: Country) => {
-          return <CountryCard key={country.name.common} country={country} />;
-        })}
-      </div>
-    </div>
+    <>
+      {" "}
+      <StCountriesContainer>
+        <h1>선택된 목록</h1>
+        <StGrid>
+          {selectedCountries.map((country: Country) => {
+            return (
+              <CountryCard
+                key={country.name.common}
+                country={country}
+                handleSelectedCountry={handleSelectedCountry}
+              />
+            );
+          })}
+        </StGrid>
+      </StCountriesContainer>
+      <StCountriesContainer>
+        <h1>나라목록</h1>
+        <StGrid>
+          {countries.map((country: Country) => {
+            return (
+              <CountryCard
+                key={country.name.common}
+                country={country}
+                handleSelectedCountry={handleSelectedCountry}
+              />
+            );
+          })}
+        </StGrid>
+      </StCountriesContainer>
+    </>
   );
 };
 
 export default CountryList;
+
+const StCountriesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  width: 100%;
+  justify-items: center;
+`;
